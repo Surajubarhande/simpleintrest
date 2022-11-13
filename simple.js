6988abfe86a53b43e1344481e208140543157c8a
Surajubarhande <surajalk1996@gmail.com>
2022-11-13 14:36:34 +0530

    added by suraj

---
+++ simple.js
@@ -0,0 +1,9 @@
+let amount,rate,year,simpleIntrest
+amount=prompt("enter your principle");
+amount=parseInt(amount);
+rate=prompt("rate of intrest");
+rate=parseInt(rate);
+year=prompt("enter year");
+year=parseInt(year);
+simpleIntrest=(amount*rate*year)/100;
+alert("simpleIntrest"+simpleIntrest)
